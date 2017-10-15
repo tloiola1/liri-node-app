eed3ed6e3962b5d6c07a558ba496ce2eab9017df
Tarciso <tarcisoloiola@hotmail.com>
2017-10-14 22:10:30 -0400

    commit 10/14 at 10:10pm

---
+++ keys.js
@@ -0,0 +1,9 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: 'dfEHiweUzQicG0KxX91tiyYkE',
+  consumer_secret: 'Om7T011lg0tV4K3KyrxY5swr68r9YwiqDraU42J2nFu9XzkhhH',
+  access_token_key: '282661299-TSJY7PXcieereA7TfwgNuE8D5Ya4aMk7hsCbDbym',
+  access_token_secret: 'UczFz6AGxHvqEdZWMp4n65EyCOrSF5PD1iAtTsj0J6B4u'
+});
+ module.exports = twitterKeys;
